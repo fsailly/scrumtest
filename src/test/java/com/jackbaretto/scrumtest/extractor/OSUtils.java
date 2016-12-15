8743f66f800a053f492ce028adac3738e63767a8
florentsailly <florent.sailly@gmail.com>
2016-12-15 22:15:36 +0100

    Code review #1 RC

--- src/test/java/com/jackbaretto/scrumtest/extractor/OSUtils.java
+++ src/test/java/com/jackbaretto/scrumtest/extractor/OSUtils.java
@@ -13,11 +13,11 @@ public class OSUtils {
     }
 
     public static boolean isMac() {
-        return (OS.indexOf("mac") >= 0);
+        return OS.contains("mac");
     }
 
     public static boolean isUnix() {
-        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+        return OS.contains("nix") || OS.contains("nux") || OS.contains("aix");
     }
 
 }
