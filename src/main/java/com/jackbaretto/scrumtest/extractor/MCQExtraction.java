937e0d65c428b4609ac6a62c0a4d8f34d57b1b0d
Mehdi-Elk <m.elketroussi@gmail.com>
2016-12-21 14:04:06 +0100

    Adding vue.js.

--- src/main/java/com/jackbaretto/scrumtest/extractor/MCQExtraction.java
+++ src/main/java/com/jackbaretto/scrumtest/extractor/MCQExtraction.java
@@ -13,8 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MCQExtraction {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(MCQExtraction.class, args);
     }
---
+++ src/main/java/com/jackbaretto/scrumtest/extractor/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.jackbaretto.scrumtest.extractor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by mehdi on 19/12/16.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+    }
+
+}
