e73643492b796ad22fb450fb9760a7c837542fba
Mehdi-Elk <m.elketroussi@gmail.com>
2016-12-26 13:36:14 +0100

    Adding simple vuejs webapp.

--- src/main/java/com/jackbaretto/scrumtest/extractor/MvcConfig.java
+++ src/main/java/com/jackbaretto/scrumtest/extractor/MvcConfig.java
@@ -1,9 +1,10 @@
 package com.jackbaretto.scrumtest.extractor;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by mehdi on 21/12/16.
@@ -12,13 +13,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/todo").setViewName("todo");
+
+        registry.addRedirectViewController("/index" ,"/index.html");
+        registry.addRedirectViewController("/" ,"/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
 
--- src/main/java/com/jackbaretto/scrumtest/security/WebSecurityConfig.java
+++ src/main/java/com/jackbaretto/scrumtest/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
@@ -34,13 +35,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+
     @Override
     protected void configure(HttpSecurity http) throws ConfigurationException {
         try {
             http
                     .authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
                     .anyRequest().authenticated()
+                    .antMatchers("/resources/**").permitAll()
                     .and()
                     .formLogin()
                     .loginPage("/login")
